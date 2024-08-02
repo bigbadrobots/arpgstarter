b318feba4c36c0a0d86ed5bd618945c8ef0a0558
Terence Tan <terence.tan@bigbadrobots.com>
2024-08-02 08:16:59 +0800

    update player script for animation

--- player.gd
+++ player.gd
@@ -5,10 +5,30 @@ extends CharacterBody2D
 
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D as AnimatedSprite2D
 
+var last_direction:Vector2 = Vector2.ZERO
+
 func _physics_process(delta):
 	var direction: Vector2 = Input.get_vector("left","right","up","down")
 	
 	velocity.x = move_toward(velocity.x, speed * direction.x,accel)
 	velocity.y = move_toward(velocity.y, speed * direction.y,accel)
 	
+	if direction != Vector2.ZERO:
+		last_direction = direction
+	
+	if direction.x != 0:
+		anim_sprite.play("walk_right")
+	elif direction.y < 0:
+		anim_sprite.play("walk_up")
+	elif direction.y > 0:
+		anim_sprite.play("walk_down")
+	else:
+		if last_direction.x != 0:
+			anim_sprite.play("idle_right")
+		elif last_direction.y < 0:
+			anim_sprite.play("idle_up")
+		elif direction.y > 0:
+			anim_sprite.play("idle_down")
+	
+	anim_sprite.flip_h = last_direction.x < 0		
 	move_and_slide()
